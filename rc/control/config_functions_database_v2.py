c2591fba85c8f9b8b415ef5b7551302e3210ebe2
John Freeman <jcfree@mu2edaq01.fnal.gov>
2017-04-20 14:43:04 -0500

    JCF: added functionality to save FHiCL info to the database with each run using the Python API

--- rc/control/config_functions_database_v2.py
+++ rc/control/config_functions_database_v2.py
@@ -1,9 +1,15 @@
+# JCF, Apr-20-2017
+
+# For this module to work, you'll first need to have set up the
+# artdaq-database in the shell environment
+
 import subprocess
 from subprocess import Popen
 
 import re
 import os
 import string
+import shutil
 
 import sys
 sys.path.append( os.getcwd() )
@@ -12,6 +18,7 @@ sys.path.append(os.environ["PYTHONPATH"] + "/../bin/")
 from rc.control.utilities import expand_environment_variable_in_string
 from conftool import exportConfiguration
 from conftool import getListOfAvailableRunConfigurationPrefixes
+from conftool import archiveRunConfiguration
 
 def config_basedir(self):
     return "/daq/database/tmp/%s" % (self.config_for_run)
@@ -34,7 +41,47 @@ def get_config_info_base(self):
 
 
 def put_config_info_base(self):
-    assert False, "%s not yet implemented" % (put_config_info_base.__name__)
+
+    scriptdir = os.environ["PWD"] + "/utils"
+
+    if not os.path.exists( scriptdir ):
+        raise Exception("Error in %s: unable to find script directory \"%s\"; should be in the base directory of the package" % (put_config_info_base.__name__, scriptdir))
+
+    runnum = str(self.run_number)
+    runrecord = self.record_directory + "/" + runnum
+
+    tmpdir = "/tmp/" + Popen("uuidgen", shell=True, stdout=subprocess.PIPE).stdout.readlines()[0].strip()
+
+    cmds = []
+    cmds.append(" scriptdir=" + scriptdir)
+    cmds.append( "mkdir " + tmpdir)
+    cmds.append( "cd " + tmpdir)
+    cmds.append( "cp -rp " + runrecord + " . ")
+    cmds.append( "chmod 777 " + runnum )
+    cmds.append( "cat " + runnum + "/metadata.txt | awk -f $scriptdir/fhiclize_metadata_file.awk > " + runnum + "/metadata.fcl" )
+    cmds.append( "rm -f " + runnum + "/*.txt")
+    cmds.append("cp -p %s/bin/schema.fcl ." % os.environ["ARTDAQ_DATABASE_FQ_DIR"])
+    
+    status = Popen( "; ".join( cmds ), shell=True).wait()
+
+    for filename in [tmpdir, "%s/%s" % (tmpdir, runnum), "%s/%s/metadata.fcl" % (tmpdir, runnum)] :
+        assert os.path.exists( filename ), "%s is unexpectedly missing" % (filename)
+
+    if status != 0:
+        raise Exception("Problem during execution of the following:\n %s" % "\n".join(cmds))
+
+
+    basedir=os.getcwd()
+    os.chdir( tmpdir )
+    archiveRunConfiguration( self.config_for_run, runnum )
+    os.chdir( basedir )
+
+    res = re.search(r"\w{8}-\w{4}-\w{4}-\w{4}-\w{12}", tmpdir)
+    assert res, "Unable to find uuidgen-generated temporary directory, will perform no deletions"
+
+    shutil.rmtree( tmpdir )
+    
+    return
 
 def listdaqcomps_base(self):
     assert False, "%s not yet implemented" % (listdaqcomps_base.__name__)
@@ -51,7 +98,8 @@ def listconfigs_base(self):
 def main():
 
     listconfigs_test = False
-    get_config_info_test = True
+    get_config_info_test = False
+    put_config_info_test = False
 
     if listconfigs_test:
         print "Calling listconfigs_base"
@@ -61,13 +109,22 @@ def main():
         print "Calling get_config_info_base"
 
         class MockDAQInterface:
-            config_for_run = "smurf"
+            config_for_run = "push_pull_testing"
 
         mydir, mydirs = get_config_info_base( MockDAQInterface() )
 
         print "FHiCL directories to search: " + " ".join(mydirs)
         print "Directory where the FHiCL documents are located: " + mydir
 
-        
+    if put_config_info_test:
+        print "Calling get_config_info_base"
+
+        class MockDAQInterface:
+            run_number = 73
+            record_directory = "/daq/artdaq/run_records/"
+            config_for_run = "push_pull_testing"
+
+        put_config_info_base( MockDAQInterface() )
+
 if __name__ == "__main__":
     main()
