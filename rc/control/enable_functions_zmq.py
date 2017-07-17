70c833ac7b560740ef8eb1d95b88ce5a7d770e0d
Enrico Gamberini <enrico.gamberini@cern.ch>
2017-07-17 22:18:47 +0200

    JCF: add in Wes's inhibitmaster functionality

---
+++ rc/control/enable_functions_zmq.py
@@ -0,0 +1,20 @@
+import zmq
+from rc.InhibitMaster import InhibitMaster
+import os, sys, time
+
+sys.path.append( os.getcwd() )
+
+def do_enable_base(self):
+    context = zmq.Context()
+    publisher = InhibitMaster.StatusPUBNode(context,"tcp://*:5556")
+    time.sleep(0.5)
+    publisher.send_status_msg("DAQINTERFACE","ENABLE","OK")
+
+
+def do_disable_base(self):
+    context = zmq.Context()
+    publisher = InhibitMaster.StatusPUBNode(context,"tcp://*:5556")
+    time.sleep(0.5)
+    publisher.send_status_msg("DAQINTERFACE","ENABLE","ERROR")
+
+
