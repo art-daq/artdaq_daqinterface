5dd04d879d69b3867071bfe20a24bbf344960e58
John Freeman <jcfree@mu2edaq01.fnal.gov>
2017-11-21 13:11:39 -0600

    JCF: modify save_run_record so that if we're using a ups-setup DAQInterface, the version rather than the commit hash is saved in the metadata

--- rc/control/save_run_record.py
+++ rc/control/save_run_record.py
@@ -80,7 +80,10 @@ def save_run_record_base(self):
     # Now save the commit hashes we determined during
     # initialization
 
-    outf.write("DAQInterface commit: %s\n" % ( self.get_commit_hash(os.environ["DAQINTERFACE_BASEDIR"]) ) )
+    if "ARTDAQ_DAQINTERFACE_VERSION" in os.environ.keys():
+        outf.write("DAQInterface commit: %s\n" % ( os.environ["ARTDAQ_DAQINTERFACE_VERSION"] ) )
+    else:
+        outf.write("DAQInterface commit: %s\n" % ( self.get_commit_hash(os.environ["DAQINTERFACE_BASEDIR"]) ) )
 
     for pkg in sorted(self.package_hash_dict.keys()):
         outf.write("%s commit: %s\n" % (pkg, self.package_hash_dict[ pkg ] ))
