cc5f7d1bdba9ea38d8188456ca2921d82bf8c405
np04 daq <np04daq@np04-srv-010.cern.ch>
2018-06-20 19:49:16 +0200

    JCF: save the known_boardreaders_list to the run record

--- rc/control/daqinterface.py
+++ rc/control/daqinterface.py
@@ -1748,6 +1748,8 @@ udp : { type : "UDP" threshold : "INFO"  port : 30000 host : "%s" }
                 self.alert_and_recover("Error in DAQInterface: a nonzero value was returned executing \"%s\"" %
                                        cmd)
                 return
+            else:
+                shutil.rmtree(self.tmp_run_record)
         else:
             self.alert_and_recover("Error in DAQInterface: unable to find temporary run records directory %s" % 
                                    self.tmp_run_record)
