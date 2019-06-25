beae469e807183bd7feb2e4910ff3d1688f7ffc1
Ron <ron@fnal.gov>
2019-06-25 15:24:45 -0500

    use -n0 in tail

--- bin/DAQInterface.sh
+++ bin/DAQInterface.sh
@@ -1,7 +1,7 @@
+#!/bin/sh
 
 
 # In the code below:
-# -The "sed" through which tail -f is piped is there so that the tail -f doesn't show lines output during the previous DAQInterface session
 # -The "nohup" is there so DAQInterface can wind down cleanly if users close its terminal
 # -The "stdbuf -oL" is there so that DAQInterface output makes it into the logfile in realtime
 # -The "<&-" is there to avoid hangs if users type in the terminal DAQInterface is running in (see Issue #21804)
@@ -13,32 +13,20 @@ export DAQINTERFACE_PORT=$(( $ARTDAQ_BASE_PORT + $DAQINTERFACE_PARTITION_NUMBER 
 
 expanded_daqinterface_logfilename=$( echo $( eval echo $DAQINTERFACE_LOGFILE ) )
 
-if [[ -e $expanded_daqinterface_logfilename ]]; then
-    previous_lines_of_logfile=$( wc -l $expanded_daqinterface_logfilename | awk '{print $1}' )
-else
+if [[ ! -e $expanded_daqinterface_logfilename ]]; then
     > $expanded_daqinterface_logfilename  # Create the logfile so tail -f, below, won't complain
-    previous_lines_of_logfile=0
 fi
 
-if (( previous_lines_of_logfile > 10 )); then
-    lines_to_delete=10
-else
-    lines_to_delete=$previous_lines_of_logfile
-fi
 
-if [[ -z $( ps aux | grep "$DAQINTERFACE_TTY.*tail -f $expanded_daqinterface_logfilename" | grep -v grep ) ]]; then
+if [[ -z $( ps aux | grep "$DAQINTERFACE_TTY.*tail -n0 -f $expanded_daqinterface_logfilename" | grep -v grep ) ]]; then
 
-   if (( lines_to_delete > 0 )); then
-       tail -f $expanded_daqinterface_logfilename | sed -r '1,'$lines_to_delete'd' & 
-   else
-       tail -f $expanded_daqinterface_logfilename &
-   fi
+    tail -n0 -f $expanded_daqinterface_logfilename &
 
 fi
 
 nohup stdbuf -oL $ARTDAQ_DAQINTERFACE_DIR/rc/control/daqinterface.py --partition-number $DAQINTERFACE_PARTITION_NUMBER --rpc-port $DAQINTERFACE_PORT <&- >> $expanded_daqinterface_logfilename 2>&1 
 
-pid=$( ps aux | grep "$DAQINTERFACE_TTY.*tail -f $expanded_daqinterface_logfilename" | grep -v grep | awk '{print $2}' )
+pid=$( ps aux | grep "$DAQINTERFACE_TTY.*tail -n0 -f $expanded_daqinterface_logfilename" | grep -v grep | awk '{print $2}' )
 
 if [[ -n $pid ]]; then
     kill $pid
@@ -46,5 +34,3 @@ fi
 
 unset pid
 unset expanded_daqinterface_logfilename
-unset previous_lines_of_logfile
-unset lines_to_delete
