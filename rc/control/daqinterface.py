7b5eaef8b02249b6952071ec5dd6a393a970c92f
John Freeman <jcfree@mu2edaq13.fnal.gov>
2020-05-14 16:16:30 -0500

    JCF: Issue #24420: if we enter a recovery either when in the running state or the stopping state, source the trace script since it's only sourced otherwise at the end of a normal stop transition

--- rc/control/daqinterface.py
+++ rc/control/daqinterface.py
@@ -1417,7 +1417,10 @@ class DAQInterface(Component):
             out_stderr = out_comm[1]
             status = out.returncode
 
-            if status != 0:
+            if status == 0:
+                self.print_log("d", "\nSTDOUT from command: \n%s" % (out_stdout), 3)
+                self.print_log("d", "\nSTDERR from command: \n%s" % (out_stderr), 3)
+            else:
                 self.print_log("e", "Error: execution of \"%s\" yielded a nonzero return value" % (cmd))
                 self.print_log("e", "\nSTDOUT from command: \n%s" % (out_stdout))
                 self.print_log("e", "\nSTDERR from command: \n%s" % (out_stderr))
@@ -2526,7 +2529,6 @@ class DAQInterface(Component):
 
         self.in_recovery = True
 
-
         if not self.called_launch_procs:
             self.print_log("i", "DAQInterface does not appear to have gotten to the point of launching the artdaq processes")
 
@@ -2537,6 +2539,9 @@ class DAQInterface(Component):
             self.complete_state_change(self.name, "recovering")
             self.print_log("i", "\n%s: RECOVER transition complete" % (date_and_time()))
             return
+
+        if self.state(self.name) == "running" or self.state(self.name) == "stopping":
+            self.execute_trace_script("stop")
 
         def attempted_stop(self, procinfo):
 
@@ -2605,6 +2610,8 @@ class DAQInterface(Component):
                             " it's possible the process no longer existed\n"))
                         
                     return
+
+                
                     
                 try:
                     procinfo.lastreturned = procinfo.server.daq.status()
