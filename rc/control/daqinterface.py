51dd07d5272a2ef4b0529ddf8a67a2041ab135bf
Eric Flumerfelt <eflumerf@fnal.gov>
2023-05-17 15:20:39 -0500

    Background msgviewer process

--- rc/control/daqinterface.py
+++ rc/control/daqinterface.py
@@ -1331,7 +1331,7 @@ class DAQInterface(Component):
         cmds.append("res=$( grep -l \"port: %d\" %s )" % (port_to_replace, msgviewer_fhicl))
         cmds.append("if [[ -n $res ]]; then true ; else false ; fi")
         cmds.append("sed -r -i 's/port: [^\s]+/port: %d/' %s" % (10005 + self.partition_number*1000, msgviewer_fhicl))
-        cmds.append("msgviewer -c %s 2>&1 > /dev/null" % (msgviewer_fhicl))
+        cmds.append("msgviewer -c %s 2>&1 > /dev/null &" % (msgviewer_fhicl))
     
         msgviewercmd = "$(" + construct_checked_command( cmds ) + ") &"
         
