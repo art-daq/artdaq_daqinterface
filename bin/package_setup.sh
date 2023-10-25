267bc3b619d2cce71d8ebc1d93fc0a1046236b78
Ron <ron@fnal.gov>
2023-10-25 16:19:10 -0500

    allow productsdir_for_bash_scripts value to be list

--- bin/package_setup.sh
+++ bin/package_setup.sh
@@ -10,7 +10,7 @@ test -z "${ARTDAQ_DAQINTERFACE_DIR-}" && { echo "Error: artdaq_daqinterface not 
 spackdir=$( sed -r -n 's/^\s*spack[_ ]root[_ ]for[_ ]bash[_ ]scripts\s*:\s*(\S+).*/\1/p' $DAQINTERFACE_SETTINGS )
 proddir=$( sed -r -n 's/^\s*productsdir[_ ]for[_ ]bash[_ ]scripts\s*:\s*(\S+).*/\1/p' $DAQINTERFACE_SETTINGS )
 
-if [[ -d $proddir ]]; then
+if [[ -d ${proddir%%:*} ]]; then
     source package_setup_ups.sh $@
 elif [[ -d $spackdir ]]; then
     source package_setup_spack.sh $@
